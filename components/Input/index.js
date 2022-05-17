e40cfb9423a7a045b6d25be37752ab0574743ea7
jose mancebo <jose.sobral@poli.ufrj.br>
2022-05-17 19:52:24 -0300

    Remove external div of this component

--- components/Input/index.js
+++ components/Input/index.js
@@ -5,23 +5,22 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 export default function InputComponent(props) {
     return (
-        <div className='search-input-bravado'>
-            <Form.Row>
-                <Form.Group as={Col}>
-                    <InputGroup className='search-input'>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text className='search-icon' >
-                                <AiOutlineSearch/>
-                            </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                            onKeyPress={(e) => {props.keyPress(e.target.value)}}
-                            type="text"
-                            placeholder="Search here.."
-                        />
-                    </InputGroup>
-                </Form.Group>
-            </Form.Row>
-        </div>
+        <Form.Row>
+            <Form.Group as={Col}>
+                <InputGroup className='robot-query-input-group'>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text className='robot-query-input-icon' >
+                            <AiOutlineSearch/>
+                        </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                        className='robot-query-input'
+                        onChange={(e) => {props.onChangeEvent(e.target.value)}}
+                        type="text"
+                        placeholder="Search here.."
+                    />
+                </InputGroup>
+            </Form.Group>
+        </Form.Row>
     )
 }
