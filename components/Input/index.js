28532d9ba63a139947dc320c7f3739eadfdb6a4d
jose mancebo <jose.sobral@poli.ufrj.br>
2022-05-17 11:08:59 -0300

    Refactor InputComponent to receive props

--- components/Input/index.js
+++ components/Input/index.js
@@ -3,19 +3,19 @@ import { Col } from 'react-bootstrap'
 import { AiOutlineSearch } from 'react-icons/ai'
 
 
-export default function InputComponent() {
+export default function InputComponent(props) {
     return (
         <div className='search-input-bravado'>
             <Form.Row>
                 <Form.Group as={Col}>
-                    <InputGroup>
+                    <InputGroup className='search-input'>
                         <InputGroup.Prepend>
-                            <InputGroup.Text>
-                                <AiOutlineSearch />
+                            <InputGroup.Text className='search-icon' >
+                                <AiOutlineSearch/>
                             </InputGroup.Text>
                         </InputGroup.Prepend>
                         <Form.Control
-                            onKeyPress={(e) => fetchRobots()}
+                            onKeyPress={(e) => {props.keyPress(e.target.value)}}
                             type="text"
                             placeholder="Search here.."
                         />
