f8c3df66b20cc3ea802c96cd16677a6db945b671
jose mancebo <jose.sobral@poli.ufrj.br>
2022-05-17 11:09:14 -0300

    Refactor ListRobts Component

--- components/ListRobots/index.js
+++ components/ListRobots/index.js
@@ -1,46 +1,56 @@
 import { useState } from 'react'
-import { Form, InputGroup, Col } from 'react-bootstrap'
-import { AiOutlineSearch } from 'react-icons/ai'
+import InputComponent from '../Input'
 import Robot from './Robot'
 
 function ListRobots({initialScope}) {
-    const [query, setQuery]  = useState(null)
-    const [robots, setRobot] = useState(initialScope)
+    const [query, setQuery]  = useState(String)
+    const [robots, setRobots] = useState(initialScope)
+    const [selectedRobots, setSelectedRobots] = useState(Array)
 
     function fetchRobots(query) {
-        fetch(`http://localhost:3000/api/robots?query=${query}`)
+        fetch(`https://bravado-api.herokuapp.com/api/robots?query=${query}`)
             .then((res) => res.json())
-            .then((data) => setRobot(data) )
+            .then((data) => setRobots(data) )
+    }
+
+    function unSelectRobot(id) {
+        let robotsHash = Object.fromEntries(
+            selectedRobots.map(e => [e["id"], e])
+        )
+
+        let indexOfSelectedRobot = selectedRobots.indexOf(robotsHash[`${id}`])
+        setRobots([...robots, selectedRobots[indexOfSelectedRobot]])
+        selectedRobots.splice(indexOfSelectedRobot, 1)
+    }
+
+    function selectRobot(id) {
+        let robotsHash = Object.fromEntries(
+            robots.map(e => [e["id"], e])
+        )
+
+        let indexOfSelectedRobot = robots.indexOf(robotsHash[`${id}`])
+        setSelectedRobots([...selectedRobots, robots[indexOfSelectedRobot]])
+        robots.splice(indexOfSelectedRobot, 1)
+    }
+
+    function addQuery(value) {
+        setQuery(value)
+        fetchRobots(query)
     }
 
     return (
         <div className='teste'>
             <div className='search-input-bravado'>
-                <Form.Row>
-                    <Form.Group as={Col}>
-                        <InputGroup className='search-input'>
-                            <InputGroup.Prepend>
-                                <InputGroup.Text className='search-icon' >
-                                    <AiOutlineSearch/>
-                                </InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <Form.Control
-                                onKeyPress={(e) => {
-                                    setQuery(e.target.value)
-                                    fetchRobots(query)
-                                }}
-                                type="text"
-                                placeholder="Search here.."
-                            />
-                        </InputGroup>
-                    </Form.Group>
-                </Form.Row>
+                <InputComponent keyPress={addQuery} />
+            </div>
+            <div className='selected-robots'>
+                {selectedRobots.map((robot, i) => <Robot {...robot} key={i} highlighted={query} selectRobot={unSelectRobot} selected={true} />)}
             </div>
             <div className='cards-section-bravado'>
-                {robots.map((robot, i) => <Robot {...robot} key={i} highlighted={query} />)}
+                {robots.map((robot, i) => <Robot {...robot} key={i} highlighted={query} selectRobot={selectRobot} selected={false} />)}
             </div>
         </div>
     )
 }
 
-export default ListRobots
+export default ListRobots 
